e86e86cdcd7d0a98adbdac2670928bfe1a0050c6
Урбанович Владислав Сергеевич <Urbanovich@belgeo.local>
2024-10-14 07:17:01 +0300

    try marge

--- configs/config.py
+++ configs/config.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-PATH_TO_VIDEO = ""#r"D:\Urban\vid\test\GOPR0064"  #"./"
+PATH_TO_VIDEO = r"E:\Urban\vid\test\GOPR0064"  #"./"
 VIDEOS = []
 FRAME_STEP = 5
 COUNT_PROCESSED_FRAMES = 0
@@ -11,8 +11,8 @@ INDEX_OF_All_FRAME = INDEX_OF_FRAME + (63600 * INDEX_OF_VIDEO)
 INDEX_OF_GPS = int(round(INDEX_OF_All_FRAME / 60, 0))
 
 INDEX_OF_SING = 0
-PATH_TO_GEOJSON = ""#r"D:\Urban\vid\20,06,24-Вишевичи+\20,04,24-Вишевичи.geojson"#r"D:\Urban\vid\test\city3.geojson"  #"./"
-PATH_TO_GPX = ""#r"D:\Urban\vid\20,06,24-Вишевичи+\20,04,24-Вишевичи.gpx"#r"D:\Urban\vid\test\07,07,20211.gpx"  #"./"#
+PATH_TO_GEOJSON = r"E:\Urban\vid\20,06,24-Вишевичи+\20,04,24-Вишевичи.geojson"#r"D:\Urban\vid\test\city3.geojson"  #"./"
+PATH_TO_GPX = r"E:\Urban\vid\20,06,24-Вишевичи+\20,04,24-Вишевичи.gpx"#r"D:\Urban\vid\test\07,07,20211.gpx"  #"./"#
 COUNT_FRAMES = 0
 
 ClASSIFIER = {}
